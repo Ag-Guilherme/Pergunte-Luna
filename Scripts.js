5db55754ba20273b0267728c00fb38c9d8a4022f
Guilherme <129906528+Ag-Guilherme@users.noreply.github.com>
2023-04-05 07:51:31 -0300

    Update Scripts.js
    Signed-off-by: Guilherme  <129906528+Ag-Guilherme@users.noreply.github.com>

--- Scripts.js
+++ Scripts.js
@@ -1,22 +1,27 @@
-const openai = new OpenAI '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';
+const openai = require('openai');
+const apiKey = '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';
+
+const openaiClient = new openai(apiKey);
 
 function generateAnswer(event) {
-	event.preventDefault();
-	const question = event.target.elements.question.value;
-	const prompt = `Question: ${question}\nAnswer:`;
-	const parameters = {
-		prompt: prompt,
-		temperature: 0.7,
-		max_tokens: 60,
-		n: 1,
-		stop: '\n',
-	};
-	openai.complete('text-davinci-002', parameters)
-		.then((response) => {
-			const answer = response.choices[0].text.trim();
-			document.getElementById('answer').innerHTML = answer;
-		})
-		.catch((error) => {
-			console.log(error);
-		});
+    event.preventDefault();
+    const question = event.target.elements.question.value;
+    const prompt = `Question: ${question}\nAnswer:`;
+    const parameters = {
+        prompt: prompt,
+        temperature: 0.7,
+        maxTokens: 60,
+        n: 1,
+        stop: '\n',
+    };
+
+    openaiClient.complete('text-davinci-002', parameters)
+        .then((response) => {
+            const answer = response.choices[0].text.trim();
+            document.getElementById('answer').innerHTML = answer;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
+
