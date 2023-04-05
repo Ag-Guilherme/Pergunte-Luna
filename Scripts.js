279b19aafc9f94aa3a4ef8ed01d06c3d4370e7dd
Guilherme <129906528+Ag-Guilherme@users.noreply.github.com>
2023-04-04 23:53:09 -0300

    Update Scripts.js
    Signed-off-by: Guilherme  <129906528+Ag-Guilherme@users.noreply.github.com>

--- Scripts.js
+++ Scripts.js
@@ -20,3 +20,47 @@ function generateAnswer(event) {
 			console.log(error);
 		});
 }
+const openai = new OpenAI('YOUR_API_KEY');
+
+function generateAnswer(event) {
+	event.preventDefault();
+	const question = event.target.elements.question.value;
+	const prompt = `Question: ${question}\nAnswer:`;
+	const parameters = {
+		prompt: prompt,
+		temperature: 0.7,
+		max_tokens: 60,
+		n: 1,
+		stop: '\n',
+	};
+	openai.complete('text-davinci-002', parameters)
+		.then((response) => {
+			const answer = response.choices[0].text.trim();
+			document.getElementById('answer').innerHTML = answer;
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
+const openai = new OpenAI('YOUR_API_KEY');
+
+function generateAnswer(event) {
+	event.preventDefault();
+	const question = event.target.elements.question.value;
+	const prompt = `Question: ${question}\nAnswer:`;
+	const parameters = {
+		prompt: prompt,
+		temperature: 0.7,
+		max_tokens: 60,
+		n: 1,
+		stop: '\n',
+	};
+	openai.complete('text-davinci-002', parameters)
+		.then((response) => {
+			const answer = response.choices[0].text.trim();
+			document.getElementById('answer').innerHTML = answer;
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
