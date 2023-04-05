045a22e80192387e0d68a56a0b1dce24b49be5de
Guilherme <129906528+Ag-Guilherme@users.noreply.github.com>
2023-04-05 10:56:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-c<section class="videos">
-  <article>
-    <a href="#"><img src="thumbnail1.jpg" alt="Miniatura do Vídeo 1"></a>
-    <h2><a href="#">Título do Vídeo 1</a></h2>
-    <p><a href="#">Nome do Canal</a> · 100.000 visualizações · 1 hora atrás</p>
-  </article>
-  <article>
-    <a href="#"><img src="thumbnail2.jpg" alt="Miniatura do Vídeo 2"></a>
-    <h2><a href="#">Título do Vídeo 2</a></h2>
-    <p><a href="#">Nome do Canal</a> · 50.000 visualizações · 2 horas atrás</p>
-  </article>
-  <!-- Adicione mais miniaturas de vídeo conforme necessário -->
-</section>
+const openai = new OpenAI'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';
+
+function generateAnswer(event) {
+	event.preventDefault();
+	const question = event.target.elements.question.value;
+	const prompt = `Question: ${question}\nAnswer:`;
+	const parameters = {
+		prompt: prompt,
+		temperature: 0.7,
+		max_tokens: 60,
+		n: 1,
+		stop: '\n',
+	};
+	openai.complete('text-davinci-002', parameters)
+		.then((response) => {
+			const answer = response.choices[0].text.trim();
+			document.getElementById('answer').innerHTML = answer;
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
